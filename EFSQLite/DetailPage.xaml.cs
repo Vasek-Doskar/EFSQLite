4757648fef1921ad2f75613aad13a023b963279d
Vasek-Doskar <doskar@spsjc.cz>
2024-03-14 11:20:01 +0100

    lambda expression

--- EFSQLite/DetailPage.xaml.cs
+++ EFSQLite/DetailPage.xaml.cs
@@ -10,7 +10,12 @@ public partial class DetailPage : ContentPage
 	public DetailPage(int id, MyContext context)
 	{
 		_context = context;
-        Student? s = _context.Students.FirstOrDefault(x => x.Id == id);
+        //Student s = _context.Students.FirstOrDefault(x => x.Id == id); // lambda varianta
+
+        Student s = (from student in context.Students
+                     where student.Id == id
+                     select student).FirstOrDefault();
+
         if (s != null)
         {
             Vystup = $"Student s Id {s.Id} jménem {s.Name} a pøijmením {s.Surname} ";
