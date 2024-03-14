5b88180dc3467fb174acbca131400bc13fbc8743
Vasek-Doskar <doskar@spsjc.cz>
2024-03-14 10:06:26 +0100

    Přidejte soubory projektu.

---
+++ EFSQLite/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using EFSQLite.Data;
+using EFSQLite.Models;
+using System.Runtime.InteropServices.ObjectiveC;
+
+namespace EFSQLite;
+
+public partial class MainPage : ContentPage
+{
+	MyContext _context;
+
+	public MainPage()
+	{
+		_context = new();
+		InitializeComponent();
+		lst.ItemsSource = _context.Students.ToList(); // připojení zdroje dat k ListView
+
+	}
+
+	private void SaveStudent(object sender, EventArgs e)
+	{
+		Student newStudent = new() 
+		{
+			Name = forName.Text,
+			Surname = forSurname.Text 
+		};
+
+		_context.Add(newStudent); // přidá záznam do Data Setu
+		_context.SaveChanges(); // Uloží data do databáze!!!
+	}
+
+
+}
+
